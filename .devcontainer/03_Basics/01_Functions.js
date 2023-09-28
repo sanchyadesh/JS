911e35c83726701213315173ea7423d76f53dafc
sanchyadesh <146314141+sanchyadesh@users.noreply.github.com>
2023-09-28 06:08:46 +0000

    Functions

---
+++ .devcontainer/03_Basics/01_Functions.js
@@ -0,0 +1,25 @@
+/*
+Array:
+*/
+// const myArray = [100, 200, 300, 500, 600]
+// function resultArray(getArray){
+// return getArray[1]
+// }
+// console.log(resultArray(myArray));
+
+const myNewArray = [100, 900, 400, 600, 800]
+function printArray(getArray){
+    return getArray[3]
+
+}
+console.log(printArray(myNewArray));
+
+//------------------------------------------------------------------
+function newArray(getMyArray){
+    return getMyArray[2]
+
+}
+console.log(newArray([100, 200, 300])); //from this statement the data will flow to parameter and return the value from statement field
+
+//--------------------------------------------------------------------------
+
